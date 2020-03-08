ca34f0beba495d1b9d610b69873cdddad3ab05d9
Ben Chatelain <ben@octop.ad>
2020-03-08 08:47:57 -0600

    🖥️ Add macOS targets

---
+++ Example/Example-macOS/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Example-macOS
+//
+//  Created by Ben Chatelain on 3/8/20.
+//  Copyright © 2020 Ben Chatelain. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ Example/Example-macOSFramework/Example_macOSFramework.h
@@ -0,0 +1,19 @@
+//
+//  Example_macOSFramework.h
+//  UnitTestingWithQuick
+//
+//  Created by Ben Chatelain on 3/8/20.
+//  Copyright © 2020 Ben Chatelain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Example_macOSFramework.
+FOUNDATION_EXPORT double Example_macOSFrameworkVersionNumber;
+
+//! Project version string for Example_macOSFramework.
+FOUNDATION_EXPORT const unsigned char Example_macOSFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Example_macOSFramework/PublicHeader.h>
+
+
